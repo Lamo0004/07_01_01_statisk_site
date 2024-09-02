c2a8c38054c40525d023dc1526126a5c31866222
Laura Mølnitz <lamo0004@stud.kea.dk>
2024-09-02 14:21:09 +0200

    produkt_v1

---
+++ produkt.js
@@ -0,0 +1,17 @@
+// https://kea-alt-del.dk/t7/api/products/1525
+
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+  .then((svar) => svar.json())
+  .then((data) => visProdukt(data));
+
+function visProdukt(produkt) {
+  console.log(produkt);
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${produkt.id}.webp`;
+  document.querySelector(".grid_1-1 h2").textContent = produkt.productdisplayname;
+  document.querySelector(".grid_1-1 .type").textContent = produkt.articletype;
+  document.querySelector(".grid_1-1 .brand").textContent = produkt.brandname;
+  document.querySelector(".grid_1-1 .pris").textContent = produkt.price;
+  document.querySelector(".info .season").textContent = produkt.season;
+  document.querySelector(".info .farve").textContent = produkt.basecolour;
+  document.querySelector(".info .aar").textContent = produkt.productionyear;
+}
